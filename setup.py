203c844557031f2ebe615a2a4da7ab541ec2e801
Justin van Wageningen <justinvwageningen@gmail.com>
2015-06-17 23:23:49 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bib2web',
+  packages = ['bib2web'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Reference manager and generate website with it using BibBase.org',
+  author = 'Justin van Wageningen',
+  author_email = 'justinvwageningen@gmail.com',
+  url = 'https://github.com/Juvawa/bib2web', # use the URL to the github repo
+  download_url = 'https://github.com/Juvawa/bib2web/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['bibtex', 'BibBase', 'Universiteit van Amsterdam'], # arbitrary keywords
+  classifiers = [],
+)

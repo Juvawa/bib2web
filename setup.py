db83841c195afbe564b4ba723735af3c14f10625
Justin van Wageningen <justinvwageningen@gmail.com>
2015-06-17 23:53:16 +0200

    new version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'bib2web',
   packages = ['bib2web'], # this must be the same as the name above
-  version = '0.12',
+  version = '0.13',
   description = 'Reference manager and generate website with it using BibBase.org',
   author = 'Justin van Wageningen',
   author_email = 'justinvwageningen@gmail.com',
   url = 'https://github.com/Juvawa/bib2web', # use the URL to the github repo
-  download_url = 'https://github.com/Juvawa/bib2web/tarball/0.12', # I'll explain this in a second
+  download_url = 'https://github.com/Juvawa/bib2web/tarball/0.13', # I'll explain this in a second
   keywords = ['bibtex', 'BibBase', 'Universiteit van Amsterdam'], # arbitrary keywords
   classifiers = [],
 )
